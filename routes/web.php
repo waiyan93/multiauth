31475f704c7e1689e7755e0b99b89bc6d636d35e
Wai Yan Htet <whispx93@gmail.com>
2018-02-07 15:12:56 +0630

    multi-auth complete

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,21 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/users/logout', 'Auth\LoginController@userLogout')->name('user.logout');
+
 Route::prefix('admin')->group(function(){
 	Route::get('/login', 'Auth\AdminLoginController@showLogin')->name('admin.login');
 	Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
 	Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
 	Route::get('/', 'AdminController@index')->name('admin.dashboard');
+
+	//Password reset routes 
+	Route::get('/password/reset', 'Auth\AdminForgotPasswordController@showLinkRequestForm')
+				->name('admin.password.request');
+	Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')
+				->name('admin.password.email');
+	Route::post('/password/reset', 'Auth\AdminResetPasswordController@reset');
+	Route::get('/password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')
+				->name('admin.password.reset');
 });
 
 
